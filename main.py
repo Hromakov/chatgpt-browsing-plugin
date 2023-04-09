a6a6d34e09947583c5f0ab52b0dc9191c9161162
Вова Хромаков <vova.hromakov@gmail.com>
2023-04-09 12:34:00 +0000

    a working implementation limiting the response to 1000 words

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-import json
 import os
 import quart
 import quart_cors
-from quart import jsonify, Response
 from quart import request
 import httpx
-from bs4 import BeautifulSoup
+from requests_html import HTML
+from bs4 import BeautifulSoup, Comment
 
-# Note: Setting CORS to allow chat.openapi.com is required for ChatGPT to access your plugin
+
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
-
 _SERVICE_AUTH_KEY = os.environ.get("_SERVICE_AUTH_KEY")
 
 
-
 def assert_auth_header(req):
-    assert req.headers.get(
-        "Authorization", None) == f"Bearer {_SERVICE_AUTH_KEY}"
-
+    assert req.headers.get("Authorization", None) == f"Bearer {_SERVICE_AUTH_KEY}"
 
 
 async def fetch_url(url):
@@ -25,19 +20,37 @@ async def fetch_url(url):
         response = await client.get(url)
         return response.text
 
-def filter_html(html):
+
+def filter_html(html, max_words=1000):
     soup = BeautifulSoup(html, 'html.parser')
-    return soup.get_text()
+
+    # Remove script and style tags
+    for tag in soup(['script', 'style']):
+        tag.decompose()
+
+    # Remove comments
+    for comment in soup.find_all(text=lambda text: isinstance(text, Comment)):
+        comment.extract()
+
+    # Get the text from the remaining tags
+    text = soup.get_text()
+    words = text.split()
+    limited_words = words[:max_words]
+    return ' '.join(limited_words)
+
+
 
 @app.route('/callURL', methods=['GET'])
 async def callURL():
-    url = quart.request.args.get('url', None)
+    url = request.args.get('url', None)
     if not url:
         return quart.Response(response='Missing URL parameter', status=400)
-    assert_auth_header(quart.request)
+
+    assert_auth_header(request)
     raw_html = await fetch_url(url)
     filtered_html = filter_html(raw_html)
     return quart.Response(response=filtered_html, status=200)
+
 
 @app.get("/logo.png")
 async def plugin_logo():
